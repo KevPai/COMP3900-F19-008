bae22e1b52500f1dcd645468488c955ee472dc34
Kevin Pai <KPai555@Hotmail.com>
2019-09-19 01:53:46 -0700

    SHADERS NOW WORKING!

--- Header.h
+++ Header.h
@@ -11,7 +11,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 const unsigned int SCR_WIDTH = 800;
--- Window.cpp
+++ Window.cpp
@@ -1,4 +1,5 @@
-#include "Shader.cpp"
+#include "Header.h"
+#include "Shader.h"
 
 int main()
 {
@@ -8,7 +9,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//Create window
+	//Create the window
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Engine 51", NULL, NULL);
 	if (window == NULL)
 	{
@@ -17,7 +18,6 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	//Initialize GLAD, GLAD manages function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -26,101 +26,75 @@ int main()
 		return -1;
 	}
 
-	//Initialize a shader
-	Shader myShader("6.1.coordinate_systems.vs", "6.1.coordinate_systems.fs");
+	//Size of rendering window
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
 
-	// set up vertex data (and buffer(s)) and configure vertex attributes
-	float vertices[] = {
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
-		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
-	};
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,  // first Triangle
-		1, 2, 3   // second Triangle
+	Shader myShader("core.vs", "core.frag");
+
+	//Shape coordinates
+	float vertices[] =
+	{
+		//Position				//Color
+		 0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f, // top right
+		 0.5f, -0.5f, 0.0f,		1.0f, 0.0f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,// bottom left
+		-0.5f,  0.5f, 0.0f,		0.0f, 0.5f, 0.5f,// top left 
 	};
 
+	unsigned int indices[] = 
+	{
+		0, 1, 3, //First Triangle
+		1, 2, 3, //Second Triangle
+	};
+
+	//Vertex buffer object AND vertex array object
 	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
-	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+
+	//Bind vertex
 	glBindVertexArray(VAO);
 
+	//Bind buffer
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	
+	//Bind indicies
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	//Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
-	// texture coord attribute
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+
+	//Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
-	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-	glBindVertexArray(0);
-
-
-	// uncomment this call to draw in wireframe polygons.
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-
-	//Size of rendering window
-	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-
-	//Lets user resize window
-	void framebuffer_size_callback(GLFWwindow * window, int width, int height);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	//Keeps window open, render loop
+	// Render loop
 	while (!glfwWindowShouldClose(window))
 	{
-		//For inputs
+		// Checks inputs
 		processInput(window);
 
-		//Color background
-		glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+		// Rendering...
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //Window color
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw shape
-		glUseProgram(myShader.shaderProgram);
-		myShader.use();
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		//Use shader and vertex array
+		myShader.Use();
+
+		glBindVertexArray(VAO);
+		//Draw object
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
-		// create transformations
-		glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		// retrieve the matrix uniform locations
-		unsigned int modelLoc = glGetUniformLocation(myShader.ID, "model");
-		unsigned int viewLoc = glGetUniformLocation(myShader.ID, "view");
-		// pass them to the shaders (3 different ways)
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-		// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
-		//myShader.setMat4("projection", projection); 
-
+		// GLFW: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// optional: de-allocate all resources once they've outlived their purpose:
-	// ------------------------------------------------------------------------
+	//Clean up objects
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
 	glDeleteBuffers(1, &EBO);
@@ -128,17 +102,4 @@ int main()
 	//Clean out GFLW on close
 	glfwTerminate();
 	return 0;
-}
-
-//Process key inputs
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
-
-//Function lets user resize window
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
 }
