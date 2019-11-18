8e8f02d6ba3f955fabbb227999d5468a37ea7060
Sebastian Bejm <125sbejm@gmail.com>
2019-11-18 14:27:30 -0800

    adding base ai files

--- TankAI.cpp
+++ TankAI.cpp
@@ -4,26 +4,44 @@ TankAI::TankAI(glm::vec3 cubePositions[], int cubesSize) {
 	for (int i = 0; i < cubesSize; i++) {
 		cubes.push_back(cubePositions[i]);
 	}
-	/*for (int i = 0; i < cubesSize; i++) {
-		std::cout << cubes[i].x << " and " << cubes[i].z << std::endl;
-	}*/
+	initGrid();
+}
+
+int TankAI::round(float f) {
+	return f + (f < 0 ? -0.5 : +0.5);
+}
+
+/*void TankAI::printGrid() {
+	for (int r = 0; r < ROW; r++) {
+		std::cout << "\t";
+		for (int c = 0; c < COL; c++) {
+			std::cout << grid[r][c] << " ";
+		}
+		std::cout << std::endl;
+	}
+}*/
+
+void TankAI::initGrid() {
+	for (int r = 0; r < ROW; r++) {
+		for (int c = 0; c < COL; c++) {
+			grid[r][c] = 1;
+		}
+	}
+
+	for (size_t i = 0; i < cubes.size(); i++) {
+		int cubeX = (int)cubes[i].x;
+		int cubeZ = (int)cubes[i].z;
+
+		grid[cubeX][cubeZ] = 0;
+	}
 }
 
 bool TankAI::isValid(float x, float z) {
 	return (x >= 0) && (x < ROW) && (z >= 0) && (z < COL);
 }
 
-bool TankAI::isUnblocked(float x, float z) {
-	for (size_t i = 0; i < cubes.size(); i++) {
-		if (cubes[i].x == x && cubes[i].z == z) {
-			return false;
-		}
-	}
-	return true;
-}
-
-bool TankAI::isDestination(float x, float z, glm::vec3 dest) {
-	if (x == dest.x && z == dest.z) {
+bool TankAI::isUnblocked(int x, int z) {
+	if (grid[x][z] == 1) {
 		return true;
 	}
 	else {
@@ -31,18 +49,23 @@ bool TankAI::isDestination(float x, float z, glm::vec3 dest) {
 	}
 }
 
-double TankAI::calculateHValue(float x, float z, glm::vec3 dest) {
-	return ((double) sqrt ((x-dest.x)*(x-dest.x) + (z-dest.z)*(z-dest.z)));
+bool TankAI::isDestination(int x, int z, glm::vec3 dest) {
+	if (x == round(dest.x) && z == round(dest.z)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-// to compare floats if they are within tolerance
-bool TankAI::fabsEqual(float a, float b) {
-	return std::fabs(a - b) < 0.0001;
+double TankAI::calculateHValue(int x, int z, glm::vec3 dest) {
+	return ((double) sqrt ((x-round(dest.x))*(x-round(dest.x)) 
+		+ (z-round(dest.z))*(z-round(dest.z))));
 }
 
 void TankAI::tracePath(Cell cellDetails[][COL], glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 dest) {
-	int row = (int) dest.x;
-	int col = (int) dest.z;
+	int row = round(dest.x);
+	int col = round(dest.z);
 
 	std::stack<glm::vec3> Path;
 
@@ -70,7 +93,7 @@ void TankAI::tracePath(Cell cellDetails[][COL], glm::vec3& position, glm::vec3& 
 
 void TankAI::move(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 dest) {
 
-	if (fabsEqual(position.x, dest.x) && fabsEqual(position.z, dest.z)) {
+	if (round(position.x == dest.x) && round(position.z == dest.z)) {
 		return;
 	}
 
@@ -88,21 +111,19 @@ void TankAI::move(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPositi
 	glm::vec3 right = glm::vec3(0.0f, 0.0f, 0.0f);
 	
 	// check if the difference between the tank's current position is within a certain distance
-	//  of the destination to go in a specified direction
-	bool diag_topleft = (fabsEqual(position.x + 1, dest.x) && fabsEqual(position.z + 1, dest.z));
-	bool diag_topright = (fabsEqual(position.x - 1, dest.x) && fabsEqual(position.z + 1, dest.z));
+	// of the destination to go in a specified direction
+	bool diag_topleft = (position.x + 1 == dest.x && position.z + 1 == dest.z);
+	bool diag_topright = (position.x - 1 == dest.x && position.z + 1 == dest.z);
 
-	bool diag_botleft = (fabsEqual(position.x + 1, dest.x) && fabsEqual(position.z - 1, dest.z));
-	bool diag_botright = (fabsEqual(position.x - 1, dest.x) && fabsEqual(position.z - 1, dest.z));
+	bool diag_botleft = (position.x + 1 == dest.x && position.z - 1 == dest.z);
+	bool diag_botright = (position.x - 1 == dest.x && position.z - 1 == dest.z);
 
-	bool vert_forward = (fabsEqual(position.x, dest.x) && fabsEqual(position.z + 1, dest.z));
-	bool vert_backward = (fabsEqual(position.x, dest.x) && fabsEqual(position.z - 1, dest.z));
+	bool vert_forward = (position.x == dest.x && position.z + 1 == dest.z);
+	bool vert_backward = (position.x == dest.x && position.z - 1 == dest.z);
 
-	bool horizontal_left = (fabsEqual(position.x + 1, dest.x) && fabsEqual(position.z, dest.z));
-	bool horizontal_right = (fabsEqual(position.x - 1, dest.x) && fabsEqual(position.z, dest.z));
-
-	//bool xAbsEqual = (fabsEqual(position.x, dest.x));
-	//bool zAbsEqual = (fabsEqual(position.z, dest.z));
+	// x positive to the left of origin
+	bool horizontal_left = (position.x + 1 == dest.x && position.z == dest.z);
+	bool horizontal_right = (position.x - 1 == dest.x && position.z == dest.z);
 
 	// checking each of the 8 directions from the current cell 
 	// note: once the algo properly works, occurances of camPosition can be removed
@@ -196,6 +217,13 @@ void TankAI::move(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPositi
 }
 
 void TankAI::performSearch(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 src, glm::vec3 dest) {
+
+	int srcX = round(src.x);
+	int srcZ = round(src.z);
+
+	int destX = round(dest.x);
+	int destZ = round(dest.z);
+
 	if (isValid(src.x, src.z) == false) {
 		std::cout << "src invalid" << std::endl;
 		return;
@@ -204,11 +232,15 @@ void TankAI::performSearch(glm::vec3& position, glm::vec3& rotation, glm::vec3& 
 		std::cout << "dest invalid" << std::endl;
 		return;
 	}
-	if (isUnblocked(src.x, src.z) == false || isUnblocked(dest.x, dest.z) == false) {
-		std::cout << "src/dest is blocked" << std::endl;
+	if (isUnblocked(srcX, srcZ) == false) {
+		std::cout << "src is blocked" << std::endl;
 		return;
 	}
-	if (isDestination(src.x, src.z, dest) == true) {
+	if (isUnblocked(destX, destZ) == false) {
+		std::cout << "dest is blocked" << std::endl;
+		return;
+	}
+	if (isDestination(srcX, srcZ, dest) == true) {
 		std::cout << "we are already at the destination" << std::endl;
 		return;
 	}
@@ -230,8 +262,8 @@ void TankAI::performSearch(glm::vec3& position, glm::vec3& rotation, glm::vec3& 
 		}
 	}
 
-	i = (int)src.x; 
-	j = (int)src.z;
+	i = srcX; 
+	j = srcZ;
 	cellDetails[i][j].f = 0.0;
 	cellDetails[i][j].g = 0.0;
 	cellDetails[i][j].h = 0.0;
--- TankAI.h
+++ TankAI.h
@@ -16,24 +16,30 @@ private:
 	static const int ROW = 7;
 	static const int COL = 9;
 
+	int grid[ROW][COL];
+
+	void initGrid();
+	int round(float f);
+
 	// check if this "cell" is is a valid cell 
 	bool isValid(float x, float z);
 
 	// check if given cell is blocked or not
-	bool isUnblocked(float x, float z);
+	bool isUnblocked(int x, int z);
 
 	// check if destination cell has been reached or not
-	bool isDestination(float x, float z, glm::vec3 dest);
+	bool isDestination(int x, int z, glm::vec3 dest);
 
 	// calculate the h value
-	double calculateHValue(float x, float z, glm::vec3 dest);
-
-	bool fabsEqual(float a, float b);
+	double calculateHValue(int x, int z, glm::vec3 dest);
 
 public:
 
 	TankAI(glm::vec3 cubePositions[], int cubesSize);
 	~TankAI() {}
+
+	//void updateGrid();
+	//void printGrid();
 
 	// trace the path from the source to destination
 	void tracePath(Cell cellDetails[][COL], glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 dest);
@@ -41,7 +47,7 @@ public:
 	// move to the next "cell" based on difference between position and dest
 	void move(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 dest);
 
-	// perform the search, will have to do this in a thread
+	// perform the search, will have to do this in a thread because loops
 	void performSearch(glm::vec3& position, glm::vec3& rotation, glm::vec3& camPosition, glm::vec3 src, glm::vec3 dest);
 
 };
